3cd90e53acedaa73aa98b8c1500822e2a7ecf298
Chirag <chhirag920@gmail.com>
2026-08-07 15:05:01 +0530

    reverse a string

---
+++ strings/reverse-string.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s;
+    getline(cin, s);
+    
+    int left = 0;
+    int right = s.size() - 1;
+    
+    while(left < right){
+        swap(s[left], s[right]);
+        left++;
+        right--;
+    }
+    cout << s;
+}
