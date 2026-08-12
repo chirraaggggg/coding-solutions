6524e13b05f74be3330c17288fb2271ce99e8ca3
Chirag <chhirag920@gmail.com>
2026-08-12 21:22:16 +0530

    sum of all integers from i to j inclusive

--- sum-of-integers-from-i-to-j.cpp
+++ sum-of-integers-from-i-to-j.cpp
@@ -1,12 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main(){
+
+int main() {
     int i, j;
     cin >> i >> j;
+
+    if(i >= j || i < 0 || j >= 1000) {
+        cout << "invalid" << endl;
+        return 0;
+    }
+
     int sum = 0;
-    for(int k = i; k<=j; k++){
+
+    for(int k = i; k <= j; k++) {
         sum += k;
     }
+
     cout << sum;
+
     return 0;
-};
+}
